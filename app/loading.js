e079859eecd3ab3997ca6ead3b1d5b91b9d7c735
upomathasin <tahsinchowdhuryupoma@gmail.com>
2024-03-27 23:48:33 +0600

    fix some problems in UI

--- app/loading.js
+++ app/loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center">
       <div className="h1 text-green-600">
-        <h1>Loading.......</h1>
+        <h1 className="text-5xl ">Loading.......</h1>
       </div>
     </div>
   );
